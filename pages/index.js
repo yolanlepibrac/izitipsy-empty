6f115444e1c47fd022e60a57d877babbd6a88942
yolanlepibrac <yolan.pibrac@gmail.comleavekjcjknvsdvgit config --global user.email yolan.pibrac@gmail.com>
2020-01-12 17:46:42 +0100

    style of table

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,16 @@
 
 import fetch from 'isomorphic-unfetch'
 import React from "react";
-//import Layout from '../components/MyLayout';
 import Link from 'next/link';
+
+//import logo from './simplyk-logo.png';
+
+/*
+import getConfig from 'next/config'
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+*/
 
 const res = [
   {price : "<20", number:220},
@@ -23,26 +31,24 @@ class HomePage extends React.Component {
   }
 
   componentDidMount = () => {
-    let tabOfTickets = this.sortTicketsArray(this.props.ticketsDb.tickets)
+    let tabOfTickets = this.sortTicketsArray(this.props.ticketsDb.tickets, 20)
     this.setState({tabOfTickets})
   }
 
-  sortTicketsArray = (tickets) => {
-
+  sortTicketsArray = (tickets, rangeOfValues) => {
     let ticketsSortedByAmount = {}
     let greatestAmount = Math.max(...tickets.map((tickets)=>tickets.amount))
-    let amount = greatestAmount + 20 - greatestAmount%20
-
+    let amount = greatestAmount + rangeOfValues - greatestAmount%rangeOfValues
     while (amount>0) {
       for (var i = 0; i < tickets.length; i++) {
-        if(tickets[i].amount>=amount-20 && tickets[i].amount<amount){
+        if(tickets[i].amount>=amount-rangeOfValues && tickets[i].amount<amount){
           if(!ticketsSortedByAmount[amount]){
             ticketsSortedByAmount[amount] = []
           }
           ticketsSortedByAmount[amount].push(tickets[i])
         }
       }
-      amount = amount-20
+      amount = amount-rangeOfValues
     }
     console.log(ticketsSortedByAmount)
     return ticketsSortedByAmount
@@ -63,42 +69,76 @@ class HomePage extends React.Component {
 
   setBankCharges = (value) => {
     this.setState({bankCharges :value})
-    let tabOfTickets = this.sortTicketsArray(this.props.ticketsDb.tickets)
+    let tabOfTickets = this.sortTicketsArray(this.props.ticketsDb.tickets, this.state.rangeOfValues)
     this.setState({tabOfTickets})
   }
 
   columnItem = (first, second, third, isLegend) => {
-    return (
-      <div id="tableLegend" style={{display:"flex", flexDirection:"row", width:500, justifyContent:"space-between"}}>
-        <p style={{fontWeight:isLegend?"bold":null, fontSize:isLegend?18:15, textAlign:"center"}}>{first}</p>
-        <p style={{fontWeight:isLegend?"bold":null, fontSize:isLegend?18:15, textAlign:"center"}}>{second}</p>
-        <p style={{fontWeight:isLegend?"bold":null, fontSize:isLegend?18:15, textAlign:"center"}}>{third}</p>
-      </div>
-    )
+
+      return (
+        <div id={isLegend?"tableLegend":"tableItem"} className="ligneItem">
+          <div className="cell"><p>{first}</p></div>
+          <div className="cell"><p>{second}</p></div>
+          <div className="cell"><p>{third}</p></div>
+          <style jsx>{`
+          .ligneItem{
+            display:flex;
+            flex-direction:row;
+            justify-content:space-around;
+            align-items:center;
+            width:600px;
+          }
+          #tableLegend{
+            background-color:rgba(53,117,65,1);
+            border-top-right-radius:10px;
+            border-top-left-radius:10px;
+            color:white;
+            font-size : 18
+          }
+          #tableItem{
+            background-color:rgba(245,245,245,1);
+            font-size : 15
+          }
+          .cell{
+            display:flex;
+            flexDirection:row;
+            flex:1;
+            textAlign:center;
+            justify-content:center;
+          }
+        `}</style>
+        </div>
+      )
+
   }
 
   setValue = (value) => {
     this.setState({rangeOfValues : value})
+    let tabOfTickets = this.sortTicketsArray(this.props.ticketsDb.tickets, value)
+    this.setState({tabOfTickets})
   }
 
 
   render() {
     return (
       <div style={{ display:"flex", flexDirection:"column", alignItems: 'center' }}>
-        <h1 style={{textAlign:"center"}} onClick={this.click}>SIMPLYK</h1>
+        <img id="logoSimplyk" src={"/images/simplyk-logo.png"} width="400px"></img>
         <h2  style={{textAlign:"center"}}>Bénéfices par prix du billet</h2>
-        <input type="number" min="0" max="10" step="0.1" value={this.state.bankCharges} onChange={e => this.setBankCharges(e.target.value)}
-        style={{position:"absolute",top:20,right:20,display:"flex",flexDirection:"row",justifyContent:"flex-end",alignItems:"center",width:100,height: 20,fontSize: 16,paddingLeft:10, borderRadius:5,}}></input>
+
+        <div id="bankChargesContainer">
+          <label>Frais banquaires</label>
+          <input id="inputBankCharge" type="number" min="0" max="10" step="0.1" value={this.state.bankCharges} onChange={e => this.setBankCharges(e.target.value)}>
+          </input>
+        </div>
 
         <div id="buttonIntervalContainer">
-          {
-            [5,10,20,50,100].map((value)=>{
+          <label>Interval de prix</label>
+          {[5,10,20,50,100].map((value)=>{
             return (
-              <button className="buttonInterval" style={{backgroundColor:this.state.rangeOfValues===value ? "rgba(53,117,65)":null}} onClick={() => this.setValue(value)}>{value}
+              <button className="buttonInterval" style={{backgroundColor:this.state.rangeOfValues===value ? "rgba(53,117,65,1)":null}} onClick={() => this.setValue(value)}>{value}
               </button>)
             })
           }
-
         </div>
 
         <div style={{width:200, height:10, backgroundColor:"black", alignItems:"center"}}></div>
@@ -115,6 +155,39 @@ class HomePage extends React.Component {
         h1,
         a {
           font-family: 'Arial';
+        }
+
+        label{
+          font-weight:bold;
+          margin-right:20px;
+        }
+
+        #logoSimplyk{
+          margin-top:50px;
+        }
+
+        #tableLegend{
+          background-color:rgba(53,117,65,1);
+        }
+
+        #bankChargesContainer{
+          position:absolute;
+          display:flex;
+          top:60px;
+          right:20px;
+        }
+
+        #inputBankCharge {
+          display:flex;
+          flex-direction:row;
+          justify-content:flex-end;
+          align-items:center;
+          width:185px;
+          height: 20px;
+          font-size: 16px;
+          padding-left:10px;
+          border-radius:5px;
+          color:rgba(53,117,65,1);
         }
 
         button {
